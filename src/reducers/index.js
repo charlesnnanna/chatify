bab034011e15e8edeeb93b09b78ea672b99eba02
Charles Uthulor <charlesnnanna1667@gmail.com>
2020-07-16 10:10:21 -0700

    Adding the message reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import user from './user'
 import contacts from './contacts'
+import messages from './messages'
 import activeUserId from './activeUserId'
 import {combineReducers} from 'redux'
 
 export default combineReducers({
     user,
     contacts,
+    messages,
     activeUserId
 })
